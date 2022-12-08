7331c034cfeccc2aa76be291ca9fbc82b2c8b913
Salvador Avelar-Lopez <100443266+salvadoravelar@users.noreply.github.com>
2022-12-07 21:31:39 -0800

    updated js and css

--- static/myscript.js
+++ static/myscript.js
@@ -13,7 +13,10 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    
+
+    document.getElementById(cityName).style.display = "block";     // Show the current tab, and add an "active" class to the button that opened the tab
+    evt.currentTarget.className += " active";
+
     let request = new XMLHttpRequest();
     request.open("GET","/getdata");
     request.send();
@@ -23,13 +26,57 @@ function openCity(evt, cityName) {
     }
 
     timezonelist = document.getElementById("time-zones");
-    document.getElementById(cityName).style.display = "block";     // Show the current tab, and add an "active" class to the button that opened the tab
-    evt.currentTarget.className += " active";
+}
+
+function loadtimezones(data){
+    timezones = data["timezones"]
+    for (let i =0; i < Object.keys(timezones).length;i++){
+        let newOption = new Option(timezones[i],timezones[i]);
+        timezonelist.add(newOption,undefined);
     }
-    function loadtimezones(data){
-        timezones = data["timezones"]
-        for (let i =0; i < Object.keys(timezones).length;i++){
-            let newOption = new Option(timezones[i],timezones[i]);
-            timezonelist.add(newOption,undefined);
+}
+
+// function redirectFilter() {
+//     location.replace("/filter.html");
+// }
+
+function applyFilter() {
+    const url = "http://127.0.0.1:5000";
+
+    var arr = []
+    filter1 = document.getElementById("state-name").value;
+    filter2 = document.getElementById("county-name").value;
+    filter3 = document.getElementById("time-zones").value;
+    filter4 = document.getElementById("climate-dd").value;
+    filter5 = document.getElementById("biome-type").value;
+    filter6 = document.getElementById("state-name").value;
+    filter7 = document.getElementById("county-name").value;
+    filter8 = document.getElementById("time-zones").value;
+    filter9 = document.getElementById("climate-dd").value;
+    filter10 = document.getElementById("biome-type").value;
+    filter11 = document.getElementById("state-name").value;
+    filter12 = document.getElementById("county-name").value;
+    filter13 = document.getElementById("time-zones").value;
+    filter14 = document.getElementById("climate-dd").value;
+    filter15 = document.getElementById("biome-type").value;
+    filter16 = document.getElementById("biome-type").value;
+    arr = [filter1, filter2, filter3, filter4, filter5, filter6, filter7, filter8, filter9, filter10, filter11, filter12, filter13, filter14, filter15, filter16]
+
+    const request = new XMLHttpRequest();
+    const method =  "GET";
+    request.open(method, url + "/parks");
+    request.send();
+    request.onload = function() {
+        data = JSON.parse(this.response);
+        var output = "";
+        for (i in data) {
+            output += "<tr>";
+            for (j in data[i]) {
+                output += "<td>" + data[i][j] + "</td>";
+            }
+            output += "</tr>"
         }
+        document.getElementById("showParks").innerHTML = output;
     }
+    request.send()
+}
